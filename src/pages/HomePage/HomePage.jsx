2211568eaa437941c7384570d025193a1412599c
kijin lee <lqizhen@naver.com>
2024-09-11 03:38:23 +1000

    New Releases slider

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {useSearchQuery} from "../../hooks/useSearchQuery"
+import NewReleases from "../../common/MusicCard/NewReleases";
+
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const {data} = useSearchQuery ({q:"name", type: "album"})
+  console.log ("data is ", data)
+
+
+  return (
+    <div>
+      <h1>Homepage</h1>
+
+      {/* 인기 앨범 슬라이드 추가 */}
+      <NewReleases />
+    </div>
+  ) 
+  
 };
 
 export default HomePage;
--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -3,6 +3,7 @@ import { getAccessToken } from "../authorization/getAccessToken";
 
 export const api = () => {
   const accessToken = getAccessToken();
+  console.log("accessToken", accessToken)
   return axios.create({
     baseURL: `https://api.spotify.com/`,
     timeout: 3000,
