be3a4093e8c08e4eea741055821c4a1645774f9b
junsehyeon <shjun1210@icloud.com>
2024-09-14 00:45:53 +0100

    서치페이지 ui 작업 완료

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container } from 'react-bootstrap';  
 import { FaSearch } from 'react-icons/fa';
-import { useLocation, useNavigate } from 'react-router-dom'; 
-import { useQuery } from '@tanstack/react-query'; 
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import './SearchPage.style.css';
 import { api } from '../../utils/api/api';
-
+import SearchForm from '../../common/SearchForm/SearchForm';  
+import MusicList from '../../common/MusicList/MusicList';    
 
 const useSearchQuery = (searchQuery) => {
   return useQuery({
-    queryKey: ['searchResults', searchQuery], 
+    queryKey: ['searchResults', searchQuery],
     queryFn: async () => {
-      const spotifyApi = api(); 
+      const spotifyApi = api();
       const response = await spotifyApi.get('/v1/search', {
         params: {
           q: searchQuery,
-          type: 'track,artist,album', 
+          type: 'track,artist,album',
           limit: 10
         }
       });
       return response.data;
     },
-    enabled: !!searchQuery 
+    enabled: !!searchQuery
   });
 };
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedTrack, setSelectedTrack] = useState(null);
+  const [tab, setTab] = useState('tracks'); 
   const location = useLocation();
   const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleResize = () => {
@@ -54,15 +56,11 @@ const SearchPage = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    refetch(); 
+    refetch();
   };
 
   const handleTrackClick = (track) => {
     setSelectedTrack(track);
-  };
-
-  const handleAlbumClick = (album) => {
-    navigate(`/albums/${album.id}`); 
   };
 
   const handlePlayerBarClick = () => {
@@ -88,91 +86,61 @@ const SearchPage = () => {
         </div>
       )}
 
-      <Container className="d-flex flex-column align-items-center mt-5">
+<Container className="search-page">
         {isMobile && (
-          <Form className="d-flex w-100 search-form" onSubmit={handleFormSubmit}>
-            <Form.Control
-              type="search"
-              placeholder="Search for a song, artist, or album" 
-              className="me-2 search-input"
-              aria-label="Search"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <Button variant="primary" className="search-button" type="submit">
-              <FaSearch className="search-icon" />
-            </Button>
-          </Form>
+          <SearchForm 
+            searchQuery={searchQuery} 
+            setSearchQuery={setSearchQuery} 
+            handleFormSubmit={handleFormSubmit} 
+          />
         )}
 
-        <div className="mt-4 w-100">
+        <div className="header-container">
+          <h1>검색 결과</h1>
+
+          {/* 트랙/앨범 버튼 */}
+          <div className="tabs-container">
+            <button
+              className={`tab-button ${tab === 'tracks' ? 'active' : ''}`}
+              onClick={() => setTab('tracks')}
+            >
+              곡
+            </button>
+            <button
+              className={`tab-button ${tab === 'albums' ? 'active' : ''}`}
+              onClick={() => setTab('albums')}
+            >
+              앨범
+            </button>
+          </div>
+        </div>
+        <div className="music-library" style={{ paddingBottom: selectedTrack ? '100px' : '0' }}>
           {searchResults ? (
             <>
-              {searchResults.tracks?.items.length > 0 && (
-                <ul>
-                  {searchResults.tracks.items.map((track) => (
-                    <li
-                      key={track.id}
-                      onClick={() => handleTrackClick(track)}
-                      style={{
-                        cursor: 'pointer',
-                        listStyle: 'none',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                        backgroundColor: selectedTrack?.id === track.id ? 'rgba(200, 200, 200, 0.5)' : 'transparent',
-                      }}
-                      role="button"
-                    >
-                      <img src={track.album.images[0]?.url} alt="album cover" width="50" />
-                      {track.name} by {track.artists.map(artist => artist.name).join(', ')}
-                    </li>
-                  ))}
-                </ul>
+              {tab === 'tracks' && searchResults.tracks?.items.length > 0 && (
+                <>
+                  <h2>곡</h2>
+                  <MusicList 
+                    items={searchResults.tracks.items} 
+                    type="track" 
+                    handleTrackClick={handleTrackClick} 
+                  />
+                </>
               )}
 
-              {searchResults.albums?.items.length > 0 && (
-                <ul>
-                  {searchResults.albums.items.map((album) => (
-                    <li
-                      key={album.id}
-                      onClick={() => handleAlbumClick(album)} 
-                      style={{
-                        cursor: 'pointer',
-                        listStyle: 'none',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                        display: 'flex', 
-                        justifyContent: 'space-between', 
-                        alignItems: 'center', 
-                      }}
-                      role="button"
-                    >
-                      <div>
-                        <img src={album.images[0]?.url} alt="album cover" width="50" />
-                        {album.name} by {album.artists.map(artist => artist.name).join(', ')}
-                      </div>
-                      <Button
-                        onClick={(e) => {
-                          e.stopPropagation(); 
-                          handleAlbumClick(album); 
-                        }}
-                        style={{
-                          fontSize: '1.5rem', 
-                          padding: '5px 10px', 
-                          border: 'none', 
-                          backgroundColor: 'transparent', 
-                          cursor: 'pointer'
-                        }}
-                      >
-                        {'>'}
-                      </Button>
-                    </li>
-                  ))}
-                </ul>
+              {tab === 'albums' && searchResults.albums?.items.length > 0 && (
+                <>
+                  <h2>앨범</h2>
+                  <MusicList 
+                    items={searchResults.albums.items} 
+                    type="album" 
+                    handleTrackClick={handleTrackClick} 
+                  />
+                </>
               )}
             </>
           ) : (
-            <p>No results found</p>
+            <p>결과가 없습니다</p>
           )}
         </div>
       </Container>
