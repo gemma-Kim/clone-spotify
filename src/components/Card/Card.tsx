7563b1bc370c6c25a246486de02e8eb41ed86740
Minseo Kim <gemmak.rok@gmail.com>
2025-06-14 10:25:07 +0900

    feat: card img, title, subtitle 조절 가능한 props 추가

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./Card.style.css";
 export interface CardProps {
   title: string;
+  titleFontSize?: number;
   subtitles: string[] | string;
+  subtitleFontSize?: number;
+  imgSize?: number;
   imgUrl: string;
   onClickHandler: () => void;
   roundImg?: boolean;
@@ -11,8 +14,11 @@ export interface CardProps {
 }
 
 const Card = ({
-  subtitles,
   title,
+  titleFontSize = 0.88,
+  subtitles,
+  subtitleFontSize = 0.67,
+  imgSize = 95,
   imgUrl,
   onClickHandler,
   buttonTitle,
@@ -20,17 +26,26 @@ const Card = ({
   align = "left",
 }: CardProps): JSX.Element => {
   return (
-    <div className="card-container" onClick={onClickHandler}>
+    <div className={`card-container`} onClick={onClickHandler}>
       {imgUrl && (
         <img
+          style={{ width: `${imgSize}%` }}
           src={imgUrl}
           alt={title}
           className={`card-img ${roundImg ? "round" : ""}`}
         />
       )}
       <div className={`card-info align-${align}`}>
-        <h2 className={`card-title align-${align} text-truncate`}>{title}</h2>
-        <p className={`card-subtitle align-${align} text-truncate`}>
+        <h2
+          style={{ fontSize: `${titleFontSize}rem` }}
+          className={`card-title align-${align} text-truncate`}
+        >
+          {title}
+        </h2>
+        <p
+          style={{ fontSize: `${subtitleFontSize}rem` }}
+          className={`card-subtitle align-${align} text-truncate`}
+        >
           {Array.isArray(subtitles) ? subtitles.join(",") : subtitles}
         </p>
         {buttonTitle && <button className="card-btn">{buttonTitle}</button>}
