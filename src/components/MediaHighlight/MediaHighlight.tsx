5b9c969ecc86f8cc5b06bea39e0bef8126e166a0
Minseo Kim <gemmak.rok@gmail.com>
2025-06-15 21:42:28 +0900

    feat: add media hightlight card component

--- src/components/MediaHighlight/MediaHighlight.tsx
+++ src/components/MediaHighlight/MediaHighlight.tsx
@@ -1,53 +1,29 @@
 import React from "react";
 import "./MediaHighlight.style.css";
-import Card from "../Card/Card";
 import { Album, ArtistDetail, Playlist, Track } from "@types";
+import PlayButton from "@features/player/PlayButton/PlayButton";
+import { getMediaInfo } from "src/utils/data/getMediaInfo";
 
 interface MediaHighlightProps {
   content: Track | Album | ArtistDetail | Playlist;
 }
 
 const MediaHighlight = ({ content }: MediaHighlightProps) => {
-  let title = "";
-  let subtitle = "";
-  let imgUrl = "";
-
-  switch (content.type) {
-    case "track":
-      const track = content as Track;
-      title = track.name;
-      subtitle = `track · ${track.artists[0].name}`;
-      imgUrl = track.album.images[0]?.url || "";
-
-      break;
-    case "album":
-      const album = content as Album;
-      title = album.name;
-      subtitle = `album · ${album.artists[0].name}`;
-      imgUrl = album.images[0]?.url || "";
-      break;
-    case "artist":
-      const artist = content as ArtistDetail;
-      title = artist.name;
-      subtitle = `artist`;
-      imgUrl = artist.images[0]?.url || "";
-      break;
-    case "playlist":
-      const playlist = content as Playlist;
-      title = playlist.name;
-      subtitle = `playlist${
-        playlist?.owner?.display_name && `· ${playlist?.owner?.display_name}`
-      }`;
-      imgUrl = playlist.images[0]?.url || "";
-      break;
-  }
+  const { title, subtitle, imgUrl } = getMediaInfo(content);
 
   return (
     <div className="container">
-      <img src={imgUrl} alt={title} className={"img"} />
-      <div>
-        <p className="title">{title}</p>
-        <p className="subtitle">{subtitle}</p>
+      <div className="wrapper">
+        <div className="media-info">
+          <img src={imgUrl} alt={title} className={"img"} />
+          <div>
+            <p className="title">{title}</p>
+            <p className="subtitle">{subtitle}</p>
+          </div>
+        </div>
+        <div className="btn-contatiner">
+          <PlayButton content={content} />
+        </div>
       </div>
     </div>
   );
