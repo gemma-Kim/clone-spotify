8340c33d5b9400fa31dfc12ce7235a7d5c445ca7
Minseo Kim <gemmak.rok@gmail.com>
2025-06-18 22:32:14 +0900

    chore: track list ui 구조 조정

--- src/features/track/TrackItem.tsx
+++ src/features/track/TrackItem.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./TrackItem.style.css";
-import "./TrackList.style.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useTrackPlayer } from "../../common/Player/TrackPlayerProvider/TrackPlayerProvider";
 import { formatDuration } from "src/utils/player/formatDuration";
@@ -14,73 +13,101 @@ interface TrackItemProps {
   showHeader?: boolean;
   showDuration?: boolean;
   showTrackNumber?: boolean;
+  showAlbumImg?: boolean;
   showAlbumName?: boolean;
 }
 
 const TrackItem = ({
-  showHeader,
   track,
-  album,
   index,
   showDuration,
   showTrackNumber,
+  showAlbumImg,
   showAlbumName,
 }: TrackItemProps) => {
   const [isHovered, setIsHovered] = useState(false);
-
   const { setTrackPlayerIsVisible } = useTrackPlayer();
   const navigate = useNavigate();
-  const location = useLocation();
 
   return (
     <div
-      className="track-row track-tab"
+      className="header track-row track-tab"
       onClick={() => setTrackPlayerIsVisible(true)}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {showTrackNumber && (
         <div className={`${showTrackNumber ? "column-index" : "hide-index"}`}>
-          <span>{index}</span>
+          {isHovered && !showAlbumImg ? (
+            <div className="hover-play-btn">
+              <PlayButton
+                content={track}
+                showBackground={false}
+                wrapperWidth="0"
+                wrapperHeight="0"
+                buttonColor="var(--color-white)"
+                btnWidth="1rem"
+                btnHeight="1rem"
+                position="relative"
+              />
+            </div>
+          ) : (
+            <span>{index}</span>
+          )}
         </div>
       )}
 
-      <div className="column-thumbnail">
-        <div
-          className="track-thumbnail"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <img
-            className="track-image"
-            src={track.album.images[0]?.url}
-            alt={track.name}
-          />
-          <div className="play-btn">
-            <PlayButton
-              // wrapperWidth={"0.001rem"}
-              content={track}
-              showBackground={false}
-              buttonColor={"var(--color-white)"}
-            />
+      <div className="column-title">
+        {showAlbumImg && (
+          <div
+            className={`column-thumbnail ${
+              !showTrackNumber
+                ? "column-thumbnail-margin-left"
+                : "column-thumbnail-no-margin"
+            }`}
+          >
+            <div
+              className="track-thumbnail"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <img
+                className="track-image"
+                src={track.album.images[0]?.url}
+                alt={track.name}
+              />
+              {isHovered && (
+                <div className="play-btn">
+                  <PlayButton
+                    content={track}
+                    showBackground={false}
+                    buttonColor="var(--color-white)"
+                    position="relative"
+                  />
+                </div>
+              )}
+            </div>
           </div>
+        )}
+
+        <div
+          className={`track-info ${showAlbumImg ? "margin-left" : "no-margin"}`}
+        >
+          <h3 className="track-name">{track.name}</h3>
+          <p className="artists-name">
+            {track.artists.map((a) => a.name).join(", ")}
+          </p>
         </div>
       </div>
-      <div className="column-title track-info">
-        <h3 className="track-name">{track.name}</h3>
-        <p className="artists-name">
-          {track.artists.map((a) => a.name).join(", ")}
-        </p>
-      </div>
 
-      <div className="column-album">
-        {showAlbumName && <span>{track.album.name}</span>}
-      </div>
+      <div className="column-info">
+        <div className={`${showAlbumName ? "column-album" : "hide-index"}`}>
+          <span>{track.album.name}</span>
+        </div>
 
-      <div className="column-duration">
-        {showDuration && (
+        <div className={`${showDuration ? "column-duration" : "hide-index"}`}>
           <span>{track.duration_ms && formatDuration(track.duration_ms)}</span>
-        )}
+        </div>
       </div>
     </div>
   );
