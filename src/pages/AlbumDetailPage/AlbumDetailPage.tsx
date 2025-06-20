436f63149f05e98126f18c83091debc49a8fb63c
Minseo Kim <gemmak.rok@gmail.com>
2025-06-20 10:38:23 +0900

    style: album detail page style refactoring

--- src/pages/AlbumDetailPage/AlbumDetailPage.tsx
+++ src/pages/AlbumDetailPage/AlbumDetailPage.tsx
@@ -1,21 +1,25 @@
-import React from "react";
 import "./AlbumDetailPage.style.css";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faPlay,
   faEllipsis,
-  faHeart,
-  faPause,
+  faCirclePlus,
+  faShuffle,
+  faPlus,
+  faCheck,
+  faPlusCircle,
+  faCheckCircle,
 } from "@fortawesome/free-solid-svg-icons";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
 import Alert from "react-bootstrap/Alert";
-import MusicTab from "../../common/MusicTab/MusicTab";
 import LoadingSpinner from "../../common/LoadingSpinner/LoadingSpinner";
 import { useTrackQuery } from "../../hooks/track/useTrackQuery";
 import { useTrackPlayer } from "../../common/Player/TrackPlayerProvider/TrackPlayerProvider";
 import { useAlbumQuery } from "src/hooks/album/useAlbumQuery";
+import TrackList from "@features/track/TrackList";
+import PlayButton from "@features/player/PlayButton/PlayButton";
+import Color from "color-thief-react";
 import { Track } from "@types";
+import { useState } from "react";
 
 const AlbumDetailPage = () => {
   const { id } = useParams();
@@ -26,95 +30,98 @@ const AlbumDetailPage = () => {
     error,
   } = useAlbumQuery(id ?? "");
 
-  const {
-    isPlaying,
-    playAlbum,
-    album,
-    trackPlayerIsVisible,
-    setTrackPlayerIsVisible,
-  } = useTrackPlayer();
+  const [isActive, setIsActive] = useState(false);
+
+  const { playAlbum, album, trackPlayerIsVisible, setTrackPlayerIsVisible } =
+    useTrackPlayer();
 
   const trackIds =
     albumData?.tracks?.items?.map((track: Track) => track.id) || [];
   const { data: trackData } = useTrackQuery(trackIds);
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-  if (isError) {
-    return <Alert variant="danger">(error.message)</Alert>;
-  }
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <Alert variant="danger">{error.message}</Alert>;
 
   const handlePlayAlbum = () => {
     playAlbum({ albumData });
-    if (!trackPlayerIsVisible) {
-      setTrackPlayerIsVisible(true);
-    }
-  };
-
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const day = date.getDate();
-    const monthYear = date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-    });
-    return `${day} ${monthYear}`;
+    if (!trackPlayerIsVisible) setTrackPlayerIsVisible(true);
   };
 
   return (
-    <div className="albumDetailPage">
-      <div
-        className="albumDetail-section"
-        style={{
-          backgroundImage: `url(${albumData?.images[0].url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="albumDetail-container">
-          <img src={albumData?.images[1].url} />
-          <div className="albumDetail-info">
-            <h3>Album Play List</h3>
-            <h1>{albumData?.artists[0].name}</h1>
-            <div className="albumDetail-info-detail">
-              <div>{formatDate(albumData?.release_date)}</div>
-              <div>{albumData?.genre}</div>
-              <div className="albumDetail-tracks">
-                {albumData?.tracks.items.length} tracks
+    <Color src={albumData.images[0].url} format="hex" crossOrigin="anonymous">
+      {({ data, loading, error: colorError }) => (
+        <div
+          className="album-detail-page"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, 
+            ${data} 0%, #000 100%)`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            color: "white",
+          }}
+        >
+          {/* 앨범 상세 상단 섹션 */}
+          <div className="album-detail-section album-fade-mask ">
+            <div className="album-detail-container">
+              <img
+                src={albumData.images[1]?.url || albumData.images[0]?.url}
+                alt="Album cover"
+              />
+              <div className="album-detail-info">
+                <h3>Album</h3>
+                <h1>{albumData.name}</h1>
+                <div className="album-detail-info-detail">
+                  {`${albumData.artists[0].name} · ${new Date(
+                    albumData.release_date
+                  ).getFullYear()} · ${albumData.tracks.items.length} tracks`}
+                </div>
               </div>
             </div>
           </div>
+
+          <div className="album-detail-content-wrapper">
+            {/* 버튼 섹션 */}
+            <div className="album-detail-btn-section">
+              <div className="album-detail-play-btn">
+                <PlayButton
+                  wrapperWidth="4rem"
+                  wrapperHeight="4rem"
+                  btnWidth="1.3rem"
+                  btnHeight="1.3rem"
+                  content={album}
+                  position={"relative"}
+                />
+              </div>
+              <div className="album-detail-basic-btn">
+                <FontAwesomeIcon icon={faShuffle} />
+              </div>
+              <div
+                className={`album-detail-basic-btn ${isActive ? "active" : ""}`}
+                onClick={() => setIsActive(!isActive)}
+              >
+                <FontAwesomeIcon icon={isActive ? faCheck : faPlusCircle} />
+              </div>
+              <div className="album-detail-basic-btn">
+                <FontAwesomeIcon icon={faEllipsis} />
+              </div>
+            </div>
+
+            {/* 트랙 리스트 */}
+            <div className="track-tabs-container">
+              {trackData?.length > 0 && (
+                <TrackList
+                  tracks={trackData}
+                  showTrackNumber={true}
+                  showAlbumHeader={false}
+                  showAlbumImg={false}
+                />
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="albumDetail-btnBar">
-        <button className="albumDetail-btnBar-play">
-          <FontAwesomeIcon
-            icon={isPlaying && id === album?.id ? faPause : faPlay}
-            onClick={() => handlePlayAlbum()}
-          />
-        </button>
-        <button className="albumDetail-btnBar-heart">
-          <FontAwesomeIcon icon={faHeart} />
-        </button>
-        <button className="albumDetail-btnBar-ellipsis">
-          <FontAwesomeIcon icon={faEllipsis} />
-        </button>
-      </div>
-      <div className="trackTabs-container">
-        <div className="track-header">
-          <span className="sharp">#</span>
-          <FontAwesomeIcon icon={faClock} className="time-icon" />
-        </div>
-        {trackData?.length > 0 ? (
-          trackData?.map((track: Track, index: number) => (
-            <MusicTab key={index} item={track} />
-          ))
-        ) : (
-          <p>There are no tracks in this album.{/* 수정 */}</p>
-        )}
-      </div>
-    </div>
+      )}
+    </Color>
   );
 };
 
