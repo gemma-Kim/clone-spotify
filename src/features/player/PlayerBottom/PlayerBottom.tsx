88662edbdebc89d4174cfea0c0a1d4734701779c
Minseo Kim <gemmak.rok@gmail.com>
2025-06-28 08:41:55 +0900

    chore: player components naming 수정

--- src/features/player/PlayerBottom/PlayerBottom.tsx
+++ src/features/player/PlayerBottom/PlayerBottom.tsx
@@ -7,11 +7,11 @@ const PlayerBottom = () => {
   const { track, album } = usePlayer();
 
   return (
-    <div className="track-player-bottom-container">
-      <div className="track-player-bottom-info-container">
+    <div className="player-bottom-container">
+      <div className="player-bottom-info-container">
         {/* 앨범 이미지 */}
         <div
-          className="track-player-bottom-album-img"
+          className="player-bottom-album-img"
           style={{
             backgroundImage: `url(${
               track?.album?.images[0].url ?? album?.images[0].url
@@ -20,22 +20,22 @@ const PlayerBottom = () => {
         ></div>
 
         {/* 트랙 정보 (트랙 이름과 아티스트) */}
-        <div className="track-player-bottom-track-info-container">
-          <div className="track-player-bottom-track-name-container">
-            {track?.name || " "}
+        <div className="player-bottom-track-info-container">
+          <div className="player-bottom-track-name-container">
+            {track?.name}
           </div>
-          <div className="track-player-bottom-track-artist-container">
-            {track?.artists?.map((artist: any) => artist?.name).join(", ") ||
-              " "}
+          <div className="player-bottom-artist-container">
+            {track?.artists &&
+              track?.artists?.map((artist: any) => artist?.name).join(", ")}
           </div>
         </div>
       </div>
 
       {/* 하단 재생 바 */}
-      <div className="track-player-bottom-controller">
+      <div className="player-bottom-controller">
         <PlayerProgressBar track={track!} />
       </div>
-      <div className="track-player-bottom-empty">.</div>
+      <div className="player-bottom-empty">.</div>
     </div>
   );
 };
